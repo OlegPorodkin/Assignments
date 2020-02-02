8d1705889916606424ee70bf9f602361289fdcb1
OlegPorodkin <p2o7r0o4d9k0in>
2020-02-03 01:05:35 +0300

    Create project

---
+++ src/test/java/ru/porodkin/assigments/AssignmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.porodkin.assigments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
